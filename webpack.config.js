8fb5429057e845d97b6cf632d19bbc7fa84bb043
Luis Boscan <lboscannava@gmail.com>
2018-12-18 09:45:00 -0300

    Initial project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+var ExtractText = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'assets/src/js/index'),
+    output: {
+        path: path.join(__dirname, 'assets/dist'),
+        filename: '[name]-[hash].js'
+    },
+    plugins: [
+        new BundleTracker({
+            path: __dirname,
+            filename: 'webpack-stats.json'
+        }),
+        new ExtractText({
+            filename: '[name]-[hash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            }, {
+                test: /\.scss$/,
+                use: ExtractText.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    }
+};
